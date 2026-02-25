e4173ad868c39a37f94b06e4737e9e105a0389fa
Vlada Em <vladaem@MacBook-Air-Vlada.local>
2026-02-26 00:36:26 +0900

    feat:Mobile version is done

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
- const swiper = new Swiper('.swiper', {
-    loop: true,
-    
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-  });
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 16,
 
-  
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+      grid: {
+        rows: 2,
+        fill: 'row'
+      },
+      allowTouchMove: false
+    },
+
+    1120: {
+      slidesPerView: 4,
+      grid: {
+        rows: 2,
+        fill: 'row'
+      },
+      allowTouchMove: false
+    }
+  }
+});
+
+const button = document.querySelector('.show-all');
+const swiperContainer = document.querySelector('.swiper');
+const buttonText = button.querySelector('button');
+
+button.addEventListener('click', () => {
+  swiperContainer.classList.toggle('expanded');
+
+  if (swiperContainer.classList.contains('expanded')) {
+    buttonText.textContent = 'Скрыть';
+  } else {
+    buttonText.textContent = 'Показать всё';
+  }
+});
